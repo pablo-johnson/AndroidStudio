619e8464a4b346c89ee693e4f0697249831b5ba1
pablo-johnson <Pablo@Mac-mini-de-Pablo.local>
2015-10-23 02:38:05 -0500

    commit inicial MultiProject
    Un multiproyecto con el modulo core como base y dos funcionalidades
    separadas en distintos modelos (location y payment). Hay un modulo que
    solo sirve para ejecutar el app que es el app

---
+++ MultiProject/multiproject-core/src/main/java/com/johnson/pablo/multiproject/core/MainCoreActivity.java
@@ -0,0 +1,13 @@
+package com.johnson.pablo.multiproject.core;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MultiProject/multiproject-location/src/main/java/com/johnson/pablo/multiproject/location/MainLocationActivity.java
@@ -0,0 +1,16 @@
+package com.johnson.pablo.multiproject.location;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.johnson.pablo.multiproject.core.MainCoreActivity;
+
+public class MainLocationActivity extends MainCoreActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
